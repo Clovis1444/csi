f83b9acbc031a6e55ac813d67858d4410d165b13
clovis <eu11111444@gmail.com>
2026-02-11 19:45:33 +0300

    feat: implement file read

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
 // Hide console window on Windows in release
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod csi_parser;
+
 fn main() {
-    println!("Hello, World!");
+    let f_path = "../samples/test.txt";
+
+    match csi_parser::read_file_as_strings(f_path) {
+        Ok(v) => {
+            let content: String = v
+                .iter()
+                .enumerate()
+                .map(|(i, val)| format!("{i} {val}\n"))
+                .collect();
+
+            println!("{f_path} content:\n{content}")
+        }
+        Err(e) => {
+            println!("Failed to read {f_path}: {e}.")
+        }
+    };
 }
