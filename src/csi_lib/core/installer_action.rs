7fa5544523efcf45bb980f24ed7ce85ac72927fe
clovis <eu11111444@gmail.com>
2026-03-01 20:01:13 +0300

    refactor: refactor InstallerAction

--- src/csi_lib/core/installer_action.rs
+++ src/csi_lib/core/installer_action.rs
@@ -1,20 +1,35 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize, Debug)]
-pub enum InstallerAction {
-    None,
-    Copy(CopyInstallerAction),
-    Download(DownloadInstallerAction),
+pub struct InstallerAction {
+    #[serde(alias = "type")]
+    action_type: InstallerActionType,
+    #[serde(alias = "source")]
+    var_source: String,
+    #[serde(alias = "cond", default)]
+    var_conds: Vec<String>,
+    output_path: String,
+}
+impl InstallerAction {
+    pub fn vars(&self) -> Vec<String> {
+        let mut var_list: Vec<String> = self.var_conds.clone();
+        var_list.push(self.var_source.clone());
+
+        return var_list;
+    }
+
+    pub fn is_valid(&self) -> bool {
+        let result = match self.action_type {
+            InstallerActionType::Copy | InstallerActionType::Download
+            => { true },
+        };
+
+        return result;
+    }
 }
 
 #[derive(Deserialize, Serialize, Debug)]
-pub struct CopyInstallerAction {
-    var_name: String,
-    output_path: String,
-}
-
-#[derive(Deserialize, Serialize, Debug)]
-pub struct DownloadInstallerAction {
-    var_name: String,
-    output_path: String,
+pub enum InstallerActionType {
+    Copy,
+    Download,
 }
